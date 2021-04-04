c624c97d15c1a3f1e82ff2474205b9765107d488
Michał Szadkowski <michalszadkowski@yahoo.pl>
2021-04-04 23:47:36 +0200

    Progress bar for download
    Progress bar (tqdm) for download process to show user,
    that something is happening during download of big images.

--- apod_everyday/downloader.py
+++ apod_everyday/downloader.py
@@ -1,4 +1,5 @@
 import requests
+from tqdm import tqdm
 
 class downloader:
 
@@ -10,9 +11,20 @@ class downloader:
 
         #Check status code, 200 for OK
         if 200 == r.status_code:
+
+            bar_divisor = 100
+            total_size = int(r.headers.get('content-length', 0))
+            block_size = total_size // bar_divisor
+            bar_format = '{l_bar}{bar:'+ f'{bar_divisor}' + '}{r_bar}{bar:-10b}'
+
+            progress_bar = tqdm(total=total_size, bar_format=bar_format)
+
             try: 
                 with open(self.download_directory + output_name, 'wb') as f:
-                    f.write(r.content)
+                    for chunk in r.iter_content(block_size):
+                        progress_bar.update(len(chunk))
+                        f.write(chunk)
+                    progress_bar.close()
                     return True
 
             except OSError as err:
